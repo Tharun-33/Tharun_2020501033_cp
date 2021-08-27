6b7dcec0c5ca7eb20370006f141f612e52a1fac1
THARUNKUMAR94 <36506286+THARUNKUMAR94@users.noreply.github.com>
2021-08-28 01:23:19 +0530

    done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -16,4 +16,30 @@
 
 
 def nth_happy_number(n):
-	return 0
+    if n==1:
+        return 1
+    if n==2:
+        return 7
+ 
+    a=2;b=8
+    while a<=n:
+        if ishappy(b):
+            a+=1
+        if a==n:
+            return b
+        b+=1
+ 
+def ishappy(m):
+    while(m>=10):
+        m=squarenum(m)
+        if(m==1):
+            return True
+    return False
+ 
+def squarenum(b):
+    sum=0
+    while(b>0):
+        rem=b%10
+        sum+=(rem*rem)
+        b//=10
+    return sum  
