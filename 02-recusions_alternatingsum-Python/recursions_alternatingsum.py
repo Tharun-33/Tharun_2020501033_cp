93e8ee59203d6476adee296f345075f8d18c7b1c
THARUNKUMAR94 <36506286+THARUNKUMAR94@users.noreply.github.com>
2021-08-28 20:31:30 +0530

    done

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -5,4 +5,15 @@
 
 
 def fun_recursions_alternatingsum(l): 
-	return 0
+    if len(l)==0:
+        return 0
+    else:
+        result= l[0] - fun_recursions_alternatingsum(l[1:])
+        return result
+ 
+def readList():
+    a=[]
+    l=int(input())
+    for i in range(l):
+        a.append(int(input()))
+    return a
