df972a7f9801309ed9d6fe3bcdc62d287837d85c
THARUNKUMAR94 <36506286+THARUNKUMAR94@users.noreply.github.com>
2021-09-02 21:02:12 +0530

    done

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -7,41 +7,40 @@
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
 # Max Iterations
-MAX_ITERATIONS = 20;
+MAX_ITERATIONS = 20
 
 # Function to check whether number is
 # Lychrel Number
 def isLychrel(number):
 	
 	for i in range(MAX_ITERATIONS):
-		number = number + reverse(number);
+		number = number + reverse(number)
 		
 		if (isPalindrome(number)):
-			return "false";
+			return "false"
 	
-	return "true";
+	return "true"
 
 # Function to check whether the number
 # is Palindrome
 def isPalindrome(number):
 
-	return number == reverse(number);
+	return number == reverse(number)
 
 # Function to reverse the number
 def reverse(number):
-
-	reverse = 0;
+	reverse = 0
 	while (number > 0):
 	
-		remainder = number % 10;
-		reverse = (reverse * 10) + remainder;
-		number = int(number / 10);
+		remainder = number % 10
+		reverse = (reverse * 10) + remainder
+		number = int(number / 10)
 	
-	return reverse;
+	return reverse
 
 # Driver Code
-number = 295;
-print(number," is lychrel? ",isLychrel(number));
+number = 295
+print(number," is lychrel? ",isLychrel(number))
 
 # This code is contributed by mits
 
